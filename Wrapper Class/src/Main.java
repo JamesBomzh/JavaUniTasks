55bea016db2d0966295215ffd537181de38e4ec4
Vladimir Istomin <56888502+JamesBomzh@users.noreply.github.com>
2020-10-23 13:39:16 +0300

    WrapperClass added

---
+++ Wrapper Class/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.add();
+        list.add();
+        list.add();
+        list.out();
+        list.pop(2);
+        list.out();
+        list.pop(1);
+        list.out();
+        list.pop(56);
+    }
+}
