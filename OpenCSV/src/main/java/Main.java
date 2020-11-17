7209b04ad7016a0ff7201c6636e9216aed0ea2e3
Vladimir Istomin <56888502+JamesBomzh@users.noreply.github.com>
2020-11-17 22:11:48 +0300

    fix 4 lab

--- OpenCSV/src/main/java/Main.java
+++ OpenCSV/src/main/java/Main.java
@@ -5,15 +5,28 @@ import java.io.IOException;
 
 
 public class Main {
-    public static  void main(String[] args) throws IOException {
+    public static  void main(String[] args) throws Exception {
         FileReader fileReader = new FileReader("C:\\Users\\JamesBomzh\\Desktop\\foreign_names.csv");
         CSVReader csvreader = new CSVReader(fileReader, ';');
         List<Human> list = new ArrayList<>();
         String[] strings;
+        try {
+            while ((strings = csvreader.readNext()) != null) {
+                list.add(new Human(strings[0], strings[1], strings[2], strings[5], strings[3], strings[4]));
 
-        while ((strings = csvreader.readNext()) != null) {
-            list.add(new Human(strings[0], strings[1], strings[2], strings[5], strings[3], strings[4]));
-
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+                if (csvreader != null) {
+                    try {
+                        csvreader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
         }
 
         for (Human human : list) {
